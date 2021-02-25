491af5b078879a0c75a42eba2e6a1ac700ceea05
Lars Karbo <mail@larskarbo.no>
2021-02-25 10:43:22 +0100

    initial

---
+++ gatsby-config.js
@@ -0,0 +1,77 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
+module.exports = {
+  siteMetadata: {
+    title: `Case Shortcut`,
+    siteUrl: `https://caseshortcut.com`,
+  },
+  plugins: [
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `content`,
+    //     path: `${__dirname}/src/content`, <- gave error on netlify, check fileparty
+    //   },
+    // },
+    'gatsby-plugin-postcss',
+    `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // Options here
+              maxWidth: 630,
+            }
+          },
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-image`,
+    // `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Imitate`,
+        short_name: `Imitate`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/pages/logo.svg`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        // exclude: [`/projects/`, `/consulting/`],
+      }
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://caseshortcut.com`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-RTPJWENTLM", // Google Analytics / GA
+        ],
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,248 @@
+import { StaticImage } from "gatsby-plugin-image";
+import React, { useEffect, useRef, useState } from "react";
+
+import { navigate } from "gatsby";
+import Helmet from "react-helmet";
+import { AiFillQuestionCircle } from "react-icons/ai";
+import { Header } from "../course/Header";
+
+export default function () {
+  return (
+    <div className="flex flex-col items-center bg-gradient-to-tr from-gray-100 pt-0 to-yellow-50 min-h-screen">
+      <Helmet>
+        <meta property="og:url" content="https://goimitate.com/" />
+        <link rel="canonical" href="https://goimitate.com/" />
+        <title>Imitate - Learn perfect pronunciation</title>
+        <meta property="og:title" content={"Imitate - Learn perfect pronunciation"} />
+        <meta name="twitter:title" content={"Imitate - Learn perfect pronunciation"} />
+        <meta name="title" content={"Imitate - Learn perfect pronunciation"} />
+
+        <meta
+          name="description"
+          content="Imitate is a pronunciation learning tool that lets you achieve near-native pronunciation with the help of real-life video."
+        />
+        <meta
+          property="og:description"
+          content="Imitate is a pronunciation learning tool that lets you achieve near-native pronunciation with the help of real-life video."
+        />
+        <meta
+          name="twitter:description"
+          content="Imitate is a pronunciation learning tool that lets you achieve near-native pronunciation with the help of real-life video."
+        />
+
+        <meta property="og:image" content="https://goimitate.com/hero.png" />
+        <meta name="twitter:image" content="https://goimitate.com/hero.png" />
+
+        <meta property="og:site_name" content="Imitate" />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta name="twitter:creator" content={"larskarbo"} />
+      </Helmet>
+
+      <div className="max-w-lg flex flex-col items-center">
+        <h1 className="text-5xl font-medium text-black text-center mt-20 mb-8">
+          <div className="mb-1">Change text case instantly.</div>
+          {/* <div className="bg-blue-200 inline-block text-blue-800 rounded-md p-2 px-4">
+            play any media in sync
+                </div> */}
+        </h1>
+        <div className="w-full font-light text-gray-900">
+          <h2 className="font-bold pt-8 text-lg pb-1">Why?</h2>
+          <p>You save multiple seconds every day. And you will feel like you are a bit better than everyone else.</p>
+
+          <h2 className="font-bold pt-8 text-lg pb-1">Will this bloat my mac?</h2>
+          <p>No! Case Shortcut is a lightweight and optimized native Mac app, written in Swift 5.</p>
+
+          <h2 className="font-bold pt-8 text-lg pb-1">Supported cases</h2>
+          <ul className="list-disc list-inside pl-4">
+            <li>This is an example senctence. (sentence case)</li>
+            <li>This Is an Example Sentence. (title case)</li>
+            <li>THIS IS AN EXAMPLE SENTENCE. (uppercase)</li>
+            <li>this is an example sentence. (lowercase)</li>
+          </ul>
+        </div>
+
+        <div className="mt-24">
+          <a href="https://larskarbo.no" target="_blank">
+            <div
+              className=" flex items-center border border-gray-200 rounded p-2 px-4
+            hover:border-gray-400 transition-colors duration-150 hover:shadow-sm
+            "
+            >
+              <img
+                alt="Lars"
+                className="rounded-full mr-2 w-8"
+                src="https://s.gravatar.com/avatar/4579b299730ddc53e3d523ec1cd5482a?s=72"
+              />
+              <div className="font-light">
+                made by <strong className="font-bold">@larskarbo</strong>
+              </div>
+            </div>
+          </a>
+          <a
+            href="https://www.producthunt.com/posts/imitate?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-imitate"
+            target="_blank"
+          >
+            <img
+              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=282675&theme=light"
+              alt="Imitate | Product Hunt"
+              //  style="width: 250px; height: 54px;"
+              className="mt-6"
+              width="250"
+              height="54"
+            />
+          </a>
+        </div>
+        <div className="pb-24"></div>
+      </div>
+    </div>
+  );
+}
+
+const languages = [
+  {
+    name: "English",
+    code: "us",
+  },
+  {
+    name: "Chinese",
+    code: "cn",
+  },
+  {
+    name: "Russian",
+    code: "ru",
+  },
+  {
+    name: "German",
+    code: "de",
+  },
+  {
+    name: "Norwegian",
+    code: "no",
+  },
+  {
+    name: "Other",
+  },
+];
+
+export function Other() {
+  const [language, setLanguage] = useState(null);
+  const [sent, setSent] = useState(false);
+  const formRef = useRef();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    fetch("/.netlify/functions/newsletter", {
+      method: "POST",
+      body: JSON.stringify({
+        email: formRef.current.email.value,
+        language: formRef.current.otherLanguage?.value || language.name.toLowerCase(),
+      }),
+    })
+      .then((a) => {
+        console.log("a: ", a);
+
+        setSent(true);
+      })
+      .catch((err) => {});
+  };
+
+  if (language) {
+    return (
+      <div className="mt-12 bg-yellow-50 border border-yellow-400 rounded px-8 py-4">
+        <button
+          onClick={() => {
+            setLanguage(null);
+            setSent(false);
+          }}
+          className="text-xs mb-4 hover:underline text-gray-800 font-light"
+        >
+          ← Go back
+        </button>
+        <form ref={formRef} onSubmit={onSubmit}>
+          <div className="flex items-center mb-4">
+            {language.code ? (
+              <img
+                className="rounded-full ring ring-gray-300 shadow "
+                src={`https://hatscripts.github.io/circle-flags/flags/${language.code}.svg`}
+                width="32"
+              />
+            ) : (
+              <AiFillQuestionCircle size={32} color="gray" />
+            )}
+            <div className="text-xl font-medium  ml-3">{language.name}</div>
+          </div>
+          {sent ? (
+            <>
+              <h2 className="text-black font-bold mb-2 ">Thank you!</h2>
+              <p className="text-black mb-2 ">I will let you know when it is available.</p>
+            </>
+          ) : language.name == "Other" ? (
+            <>
+              <h2 className="text-black font-bold mb-2 ">Which language would you like Imitate for?</h2>
+              <div className=" mb-2 flex border border-gray-300 w-96 rounded overflow-hidden text-sm">
+                <input
+                  required
+                  name="otherLanguage"
+                  className="flex-grow px-4 py-2"
+                  type="texts"
+                  placeholder="A language..."
+                />
+              </div>
+              <div className=" mb-4 flex border border-gray-300 w-96 rounded overflow-hidden text-sm">
+                <input required name="email" className="flex-grow px-4 py-2" type="email" placeholder="Your email" />
+              </div>
+              <button
+                className=" mb-2 px-4 py-2 bg-yellow-100 hover:bg-yellow-200 border border-gray-400 hover:border-gray-500 shadow-sm text-sm rounded transition-colors"
+                type="submit"
+              >
+                Get notified ⚡
+              </button>
+            </>
+          ) : (
+            <>
+              <h2 className="text-black font-bold mb-2 ">Imitate for {language.name} is not yet available.</h2>
+              <p className="text-black mb-2 ">Enter your email to be one of the first to try it.</p>
+              <div className=" mb-4 flex border border-gray-300 w-96 rounded overflow-hidden text-sm">
+                <input required name="email" className="flex-grow px-4 py-2" type="email" placeholder="Your email" />
+              </div>
+              <button
+                className=" mb-2 px-4 py-2 bg-yellow-100 hover:bg-yellow-200 border border-gray-400 hover:border-gray-500 shadow-sm text-sm rounded transition-colors"
+                type="submit"
+              >
+                Sign up for the {language.name} wait list ⚡
+              </button>
+            </>
+          )}
+          {/* <p className="text-xs font-light opacity-60 mb-2 mt-1 ">Currently available for French learners only.</p> */}
+        </form>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <h3 className="mt-12 mb-4 text-lg font-medium text-center">Other languages:</h3>
+      <div className="flex flex-wrap justify-center">
+        {languages.map((l) => (
+          <button
+            onClick={() => setLanguage(l)}
+            key={l.name}
+            className="bg-gray-50 border border-gray-400 rounded px-8 py-4 flex items-center mr-4 mb-4 w-5/12 hover:bg-gray-100 transition-colors"
+          >
+            {l.code ? (
+              <img
+                className="rounded-full ring ring-gray-300 shadow "
+                src={`https://hatscripts.github.io/circle-flags/flags/${l.code}.svg`}
+                width="32"
+              />
+            ) : (
+              <AiFillQuestionCircle size={32} color="gray" />
+            )}
+            <div className="text-xl font-light text-gray-900 ml-3 mr-4">{l.name}</div>
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}
