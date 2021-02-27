c57fdf4d0add195327f9369cd78a7703deda3a4d
Lars Karbo <mail@larskarbo.no>
2021-02-27 12:16:25 +0100

    add images

--- src/pages/welcomescreen.tsx
+++ src/pages/welcomescreen.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./welcomescreen.css";
 import { Key } from "../components/Demo";
+import { StaticImage } from "gatsby-plugin-image";
+import { Helmet } from 'react-helmet';
 
 export default function () {
   return (
     <div className="flex flex-col items-center bg-gradient-to-tr from-gray-100 pt-0 to-yellow-50 min-h-screen">
+      <Helmet>
+        <title>Welcome to Case Shortcut</title>
+      </Helmet>
+
       <div className="max-w-2xl">
         <div className="py-12 font-light max-w-lg m-auto text-center">
           I couldn't figure out how to make the native app window work, so just pretend this one is real ↓
@@ -45,33 +51,32 @@ export default function () {
               If you didn't put the .app file in Applications, go ahead and quit Case Shortcut, move it there and
               restart the app.
             </p>
+            <StaticImage className="max-w-md" src="moveapp.png" alt="Move the app" />
             <h3 className="py-2 text-xl font-medium">2. Permissions</h3>
             <p className="py-2 ">
               Case Shortcut need some permissions to be able to work across different apps as a global shortcut.
             </p>
             <p className="py-2 ">
               Go to <i className="font-light">"Settings"</i> → <i className="font-light">"Security & privacy"</i> →{" "}
-              <i className="font-light">"Accessibility"</i> and check Case Shortcut.
+              <i className="font-light">"Privacy"</i> → <i className="font-light">"Accessibility"</i> and check Case
+              Shortcut.
             </p>
+            <StaticImage className="max-w-md" src="permissions.png" alt="Permissions" />
             <h2 className="py-2 text-2xl font-semibold">Usage</h2>
-
 
             <p className="py-2 pb-4">
               Go to a textbox in any app, and mark some text. Then press on of these combinations:
             </p>
             <div className="flex items-center">
-                <Key className="mr-4" letter="⌃"></Key>
-                <Key className="mr-4" letter="⌥"></Key>
-                <Key className="mr-4" letter="⌘"></Key>
-                <div className="mr-4">+</div>
-                <Key className="mr-4" letter="▲"></Key>
-                <div className="mr-4">or</div>
-                <Key className="mr-4" letter="▼"></Key>
+              <Key className="mr-4" letter="⌃"></Key>
+              <Key className="mr-4" letter="⌥"></Key>
+              <Key className="mr-4" letter="⌘"></Key>
+              <div className="mr-4">+</div>
+              <Key className="mr-4" letter="▲"></Key>
+              <div className="mr-4">or</div>
+              <Key className="mr-4" letter="▼"></Key>
             </div>
             <textarea className="rounded p-4 my-4 border bg-white">You can trY iT hERE!</textarea>
-            <p className="py-2 ">
-              gif
-            </p>
             <h2 className="py-2 text-2xl font-semibold">Feedback</h2>
             <p className="py-2 ">
               If you have feedback, suggestions, questions or problems, reach out to me{" "}
